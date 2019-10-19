6372a8dc5b36adf8e85d772970d1c6ddbebc83c4
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2019-10-19 02:53:52 +0200

    added home route

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// routes
+const home = require("./routes/home");
+const games = require("./routes/game");
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// api endpoints
+app.use("/", home);
+app.use("/api/games", games);
+
 app.listen(PORT, debug(`App is running on port ${PORT}`));
