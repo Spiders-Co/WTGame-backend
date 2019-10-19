de08b0f35805dd6d4b2edd324237823e4a61c399
Hossam Elbahrawy <hossamelbahrawy52@gmail.com>
2019-10-19 18:59:16 +0200

    added some development config

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-require("dotenv").config();
-const debug = require("debug")("app:StartUp");
-const express = require("express");
+require('dotenv').config();
+const StartDebug = require('debug')('app:startUp');
+const devDebug = require('debug')('app:dev');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+if (app.get('env') === 'development') {
+  StartDebug(app.get('env'));
+  app.use(morgan('tiny'));
+  StartDebug('Morgan Enabled...');
+}
 // routes
-const home = require("./routes/home");
-const games = require("./routes/game");
+const home = require('./routes/home');
+const games = require('./routes/game');
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 // api endpoints
-app.use("/", home);
-app.use("/api/games", games);
+app.use('/', home);
+app.use('/api/games', games);
 
-app.listen(PORT, debug(`App is running on port ${PORT}`));
+app.listen(PORT, StartDebug(`App is running on port ${PORT}`));
