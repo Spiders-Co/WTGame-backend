fdb4550dd670771ba822790d205945be0e4c5618
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2019-10-20 17:46:37 +0200

    added ( get games) ,( post game)

--- controllers/game.js
+++ controllers/game.js
@@ -1,1 +1,22 @@
-const mongoose = require("mongoose");
+const gameDebug = require("debug")("app:game-control");
+const { gameModel } = require("../models/game");
+
+const getGames = async () => {
+  return await gameModel.find();
+};
+
+const createGame = async data => {
+  const newGame = new gameModel(data);
+  try {
+    const result = await newGame.save();
+    // gameDebug(`created game : ${result}`);
+    return result;
+  } catch (err) {
+    throw new Error(`create Error : ${err.message}`);
+  }
+};
+
+module.exports = {
+  createGame,
+  getGames
+};
--- routes/game.js
+++ routes/game.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { validate } = require("../models/game");
+const Game = require("../controllers/game");
+
+router.get("/", async (req, res) => {
+  res.send(await Game.getGames());
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    //     const errorMsgs = error.details.reduce(item => {
+    //       return acc + item.message;
+    //     }, (acc = ""));
+    return res.status(400).send(error.details.map(item => item.message));
+  }
+
+  const game = await Game.createGame(req.body);
+  res.send(game);
+});
 
 module.exports = router;
