ec32ea80748ce0d67a709b3ab13f66ab7597f785
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2019-10-20 17:45:41 +0200

    configured mongoAtlas

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-require('dotenv').config();
-const StartDebug = require('debug')('app:startUp');
-const devDebug = require('debug')('app:dev');
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const StartDebug = require("debug")("app:startUp");
+const devDebug = require("debug")("app:dev");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-if (app.get('env') === 'development') {
-  StartDebug(app.get('env'));
-  app.use(morgan('tiny'));
-  StartDebug('Morgan Enabled...');
+if (app.get("env") === "development") {
+  StartDebug(app.get("env"));
+  app.use(morgan("tiny"));
+  StartDebug("Morgan Enabled...");
 }
+
+// Connection to MongoDB
+// connection string from mlab
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.CLUSTER_USER}:${process.env.CLUSTER_PASSWORD}@${process.env.CLUSTER_NAME}-hv03g.mongodb.net/test?retryWrites=true&w=majority`
+  )
+  .then(res => devDebug("Connected to MLab Successfully ...."))
+  .catch(err => devDebug(`Error Connection : ${err.message}`));
+
 // routes
-const home = require('./routes/home');
-const games = require('./routes/game');
+const home = require("./routes/home");
+const games = require("./routes/game");
 
 // middlewares
 app.use(express.json());
@@ -23,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
 // api endpoints
-app.use('/', home);
-app.use('/api/games', games);
+app.use("/", home);
+app.use("/api/games", games);
 
 app.listen(PORT, StartDebug(`App is running on port ${PORT}`));
