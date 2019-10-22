c227067a3ac95adac54d7b10604057c7750c416b
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2019-10-22 02:15:01 +0200

    added get,update routes for games

--- controllers/game.js
+++ controllers/game.js
@@ -1,22 +1,49 @@
 const gameDebug = require("debug")("app:game-control");
 const { gameModel } = require("../models/game");
+const mongoose = require("mongoose");
 
-const getGames = async () => {
+const getAll = async () => {
   return await gameModel.find();
 };
 
-const createGame = async data => {
-  const newGame = new gameModel(data);
+const getOne = async id => {
+  const game = await gameModel.find({
+    _id: mongoose.Types.ObjectId(id)
+  });
+  if (!game) return;
+  return game;
+};
+
+const create = async game => {
+  const newGame = new gameModel(game);
   try {
     const result = await newGame.save();
-    // gameDebug(`created game : ${result}`);
     return result;
   } catch (err) {
     throw new Error(`create Error : ${err.message}`);
   }
 };
 
+const update = async (id, newGame) => {
+  const game = await gameModel.findByIdAndUpdate(
+    id,
+    { $set: newGame },
+    { new: true }
+  );
+  if (!game) return;
+  return game;
+};
+
+const deleteOne = async id => {
+  const game = await gameModel.findByIdAndDelete(id);
+  if (!game) return;
+  return game;
+};
+
 module.exports = {
-  createGame,
-  getGames
+  create,
+  getAll,
+  getOne,
+  deleteOne,
+  update
 };
--- routes/game.js
+++ routes/game.js
@@ -4,7 +4,14 @@ const { validate } = require("../models/game");
 const Game = require("../controllers/game");
 
 router.get("/", async (req, res) => {
-  res.send(await Game.getGames());
+  res.send(await Game.getAll());
+});
+
+router.get("/:id", async (req, res) => {
+  const game = await Game.getOne(req.params.id);
+  if (!game || game.length === 0)
+    return res.status(404).send("Game Not Found ...");
+  res.send(game);
 });
 
 router.post("/", async (req, res) => {
@@ -16,7 +23,24 @@ router.post("/", async (req, res) => {
     return res.status(400).send(error.details.map(item => item.message));
   }
 
-  const game = await Game.createGame(req.body);
+  const game = await Game.create(req.body);
+  res.send(game);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error)
+    return res
+      .status(400)
+      .send(error.details.map(item => item.message)[0].trim());
+  const game = await Game.update(req.params.id, req.body);
+  res.send(game);
+});
+
+router.delete("/:id", async (req, res) => {
+  const game = await Game.deleteOne(req.params.id);
+  if (!game || game.length === 0)
+    return res.status(404).send("Game Not Found ...");
   res.send(game);
 });
 
